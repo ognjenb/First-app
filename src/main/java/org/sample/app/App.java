2742309033f2dcd42dd4ede68b6ed467d887160e
Ognjen Bubalo <ognjen.bubalo@gmail.com>
2012-06-21 16:02:36 +0200

    plus package plus class plus branch

--- src/main/java/org/sample/app/App.java
+++ src/main/java/org/sample/app/App.java
@@ -1,5 +1,7 @@
 package org.sample.app;
 
+import org.sample.app.ogi.TestClass;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        TestClass a = new TestClass();
     }
 }
---
+++ src/main/java/org/sample/app/ogi/TestClass.java
@@ -0,0 +1,14 @@
+package org.sample.app.ogi;
+
+public class TestClass {
+	
+	public TestClass() {
+		boolean a = true;
+		if (a) {
+			System.out.println("ok");
+		} else {
+			System.out.println("notok");
+		}
+	}
+
+}
