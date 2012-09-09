f41540896517a12bfd936009eff16816cf59af00
Ognjen Bubalo <ognjen.bubalo@gmail.com>
2012-09-09 12:56:02 +0200

    more branches added

--- src/main/java/org/sample/app/ogi/Agave.java
+++ src/main/java/org/sample/app/ogi/Agave.java
@@ -9,7 +9,28 @@ public class Agave {
 		for (int i=a;i<b;++i) {
 			if (((b % a) == 2)) {
 				//donothing
-			}
+			} else if (((b % a) == 3)) {
+				//donothing
+			} else if (((b % a) == 5)) {
+				//donothing
+			} else if (((b % a) == 7)) {
+				//donothing
+			} else if (((b % a) == 11)) {
+				//donothing
+			} else if (((b % a) == 13)) {
+				//donothing
+			} else if (((b % a) == 17)) {
+				//donothing
+			} else if (((b % a) == 19)) {
+				//donothing
+			} else if (((b % a) == 23)) {
+				//donothing
+			} else if (((b % a) == 29)) {
+				//donothing
+			} else if (((b % a) == 31)) {
+				//donothing
+			} else if (((b % a) == 37)) {
+				//donothing
 			
 		}
 		return 3;
